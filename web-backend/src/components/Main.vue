1652facf39c63c352d46d95c6711ed2c604ed8a3
zuowenjie <zuowenjie@sunlands.com>
2019-04-10 17:14:56 +0800

    base structure complete

--- web-backend/src/components/Main.vue
+++ web-backend/src/components/Main.vue
@@ -23,25 +23,12 @@
                 </el-menu-item-group>
               </el-submenu>
               <el-submenu index="2">
-                <template slot="title"><i class="el-icon-menu"></i>订单管理</template>
-                <el-menu-item-group>
-                  <el-menu-item index="2-1">订单列表</el-menu-item>
-                </el-menu-item-group>
+                <template slot="title"><i class="el-icon-tickets"></i>消费明细</template>
+                <el-menu-item>月消费明细</el-menu-item>
               </el-submenu>
               <el-submenu index="3">
-                <template slot="title"><i class="el-icon-setting"></i>积分管理</template>
-                <el-menu-item-group>
-                  <template slot="title">分组一</template>
-                  <el-menu-item index="3-1">选项1</el-menu-item>
-                  <el-menu-item index="3-2">选项2</el-menu-item>
-                </el-menu-item-group>
-                <el-menu-item-group title="分组2">
-                  <el-menu-item index="3-3">选项3</el-menu-item>
-                </el-menu-item-group>
-                <el-submenu index="3-4">
-                  <template slot="title">选项4</template>
-                  <el-menu-item index="3-4-1">选项4-1</el-menu-item>
-                </el-submenu>
+                <template slot="title"><i class="el-icon-document"></i>统计报表</template>
+                <el-menu-item>报表</el-menu-item>
               </el-submenu>
             </el-menu>
           </el-aside>
@@ -81,7 +68,7 @@
   }
 
   .el-main {
-    background-color: #E9EEF3;
+    background-color: #fdfbff;
     color: #333;
     text-align: center;
     line-height: 160px;
