f20f3421e4bdc9263f553c22f87db3c82c789660
zuowenjie <zuowenjie@sunlands.com>
2019-04-12 19:12:54 +0800

    UserList.vue 这是浏览器导航栏icon

--- web-backend/build/webpack.dev.conf.js
+++ web-backend/build/webpack.dev.conf.js
@@ -55,6 +55,7 @@ const devWebpackConfig = merge(baseWebpackConfig, {
     new HtmlWebpackPlugin({
       filename: 'index.html',
       template: 'index.html',
+      favicon: 'src/assets/web-icon-57x57.png',//设置icon图标
       inject: true
     }),
     // copy custom static assets
--- web-backend/src/components/content/UserList.vue
+++ web-backend/src/components/content/UserList.vue
@@ -5,23 +5,27 @@
         <el-breadcrumb-item><i class="el-icon-date"></i> 用户管理</el-breadcrumb-item>
         <el-breadcrumb-item>用户列表</el-breadcrumb-item>
       </el-breadcrumb>
-      <div>
-        <el-form>
+      <div style="width: 100%; height: 95px">
+        <el-form style="margin-top: 18px">
           <el-date-picker v-model="currentDate" type="date" placeholder="time" style="float: left;"></el-date-picker>
           <el-button style="float: left; margin-left: 20px; margin-top: 10px; color: #3b97d1" v-on:click="search">查询</el-button>
         </el-form>
       </div>
       <div class="cantainer">
-        <el-table :data="userData" v-if="userData.length > 0" style="width: 100%">
-          <el-table-column fixed width="100px">
-            <template slot="header" slot-scope="scope">
-              <el-tooltip content="名称" placement="top">
-                <div class="tb-header">名称</div>
-              </el-tooltip>
-            </template>
-            <template slot-scope="scope"></template>
-          </el-table-column>
-        </el-table>
+        <div  v-if="userData.length > 0">
+          <el-table :data="userData"style="width: 100%; margin-top: -10px" height="650">
+            <el-table-column fixed width="100px" prop="name">
+              <template slot="header" slot-scope="scope">
+                <el-tooltip content="名称" placement="top">
+                  <div class="tb-header">名称</div>
+                </el-tooltip>
+              </template>
+            </el-table-column>
+          </el-table>
+          <div>
+            <el-pagination background layout="sizes, prev, pager, next" :total="userData.length"></el-pagination>
+          </div>
+        </div>
         <div  v-else style="text-align: center; width: 100%;">
           <span>暂无数据</span>
         </div>
@@ -37,7 +41,20 @@
       data() {
         return {
           currentDate: '',
-          userData: []
+          userData: [{'name': 'a'},
+            {'name': 'a'},
+            {'name': 'a'},
+            {'name': 'a'},
+            {'name': 'a'},
+            {'name': 'a'},
+            {'name': 'a'},
+            {'name': 'a'},
+            {'name': 'a'},
+            {'name': 'a'},
+            {'name': 'a'},
+            {'name': 'a'},
+            {'name': 'a'},
+            {'name': 'a'}]
         }
       },
       methods: {
