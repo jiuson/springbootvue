0ffe461e1db3876c934000159e8039a7838df1f3
wenjiezuo <zwj598507757@163.com>
2019-04-18 18:18:43 +0800

    modify

--- springboot-jpa/src/main/db/createTable.sql
+++ springboot-jpa/src/main/db/createTable.sql
@@ -1,28 +1,28 @@
 CREATE TABLE `tb_user` (
-  `id` bigint(10) NOT NULL AUTO_INCREMENT,
-  `username` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '用户名',
-  `password` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT 'md5加密后的密码',
-  `address` text NOT NULL,
-  `create_date` datetime NOT NULL COMMENT '创建时间',
-  `modify_date` datetime NOT NULL COMMENT '修改时间',
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) COLLATE utf8mb4_bin NOT NULL,
+  `password` varchar(255) COLLATE utf8mb4_bin NOT NULL,
+  `address` varchar(255) COLLATE utf8mb4_bin NOT NULL,
+  `create_date` datetime NOT NULL,
+  `modify_date` datetime NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=29 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=30 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin
 
 CREATE TABLE `tb_token` (
-  `id` bigint(10) NOT NULL AUTO_INCREMENT,
-  `user_id` bigint(10) NOT NULL COMMENT 'tb_user_id',
-  `token` varchar(255) DEFAULT NULL COMMENT 'token',
-  `create_date` datetime DEFAULT NULL COMMENT '创建时间',
-  `expire_date` datetime DEFAULT NULL COMMENT '过期时间（一个月）',
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `create_date` datetime NOT NULL,
+  `expire_date` datetime NOT NULL,
+  `token` varchar(255) COLLATE utf8mb4_bin NOT NULL,
+  `user_id` bigint(20) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=35 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin
 
 CREATE TABLE `tb_consume` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `consume_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `consume_name` varchar(255) COLLATE utf8mb4_bin NOT NULL,
   `expenditure` float NOT NULL,
-  `consume_type` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `consume_type` varchar(255) COLLATE utf8mb4_bin NOT NULL,
   `consume_date` datetime NOT NULL,
   `modify_date` datetime NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=50 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin
